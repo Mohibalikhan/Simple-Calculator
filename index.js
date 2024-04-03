616074aa53952f584d8eeb210b8651cd6515a601
Mohibalikhan <khanmohibali1@gmail.com>
2024-04-04 01:45:30 +0500

    push my calculator to github

---
+++ index.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+//Above line is called SHABANG
+
+import inquirer from "inquirer"; //inquirer sawal pochta ha
+//([{iska andr ham wo likh ge jo hame user se chaie hoga}])
+//await matlab ruko user se sawal ke jawab lene do phir age jana
+const answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "FirstNumber" },
+    //real calculator me phela number leke pochta ha ke iske uper konsa operation perform karna ha
+    { message: "Select one of the operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Division", "Multiplication"] },
+    //choices:["+","-","/","*"]},// we also use this for better understanding 
+    { message: "Enter Second Number", type: "number", name: "SecondNumber" },
+]);
+//then we use conditional statements
+if (answer.operator === "Addition") {
+    console.log(answer.FirstNumber + answer.SecondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.FirstNumber - answer.SecondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.FirstNumber / answer.SecondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}
+else {
+    console.log("You Enter valid number");
+}
